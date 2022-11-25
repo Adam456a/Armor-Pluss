2c0f95d27d3848dcb5874adc90f3a11fedd06888
Adam456a <adammccombe999@gmail.com>
2022-11-25 20:02:38 +0000

    this is just an upload

---
+++ src/main/java/net/adam/armor/armorclient.java
@@ -0,0 +1,10 @@
+package net.adam.armor;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class armorclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
